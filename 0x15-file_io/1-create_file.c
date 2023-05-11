f1a4da0c059872c41330e7a7595a649bc53a0066
“fasikashimel” <fasikashimels21@gmail.com>
2023-05-11 11:55:32 -0700

    fasika

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * create_file - creates a file
@@ -35,4 +35,4 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 
 	return (1);
-}
+}}
